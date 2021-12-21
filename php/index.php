52e87778ed8d85ab5641b75edb92f4a601fe15df
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-12-21 13:11:21 +0100

    first commit

---
+++ php/index.php
@@ -0,0 +1,22 @@
+<?php
+    require_once __DIR__ . '/data.php';
+    // var_dump(__DIR__);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <header>
+        
+    </header>
+    <main>
+
+    </main>
+</body>
+</html>
